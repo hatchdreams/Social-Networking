97bba21651b65a97aa4882d111a6ebb60e94b503
Thomas Vitale <Thomas.Vitale@hgv.com>
2020-07-16 18:59:33 -0400

    End Of Section 4

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -23,6 +23,7 @@ namespace DatingApp.API.Controllers
         }
 
         // GET api/values
+        [AllowAnonymous]  //Example for no token needed
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
